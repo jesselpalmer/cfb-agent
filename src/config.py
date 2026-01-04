f16a02bd236d5f7ea9a78d965bee5942aeb41b98
Jesse Palmer <jesselpalmer@gmail.com>
2026-01-04 15:05:48 -0800

    refactor: create default model const

---
+++ src/config.py
@@ -0,0 +1,9 @@
+"""
+Configuration constants for the CFB Agent system.
+
+This module contains default values and configuration constants that can be
+easily modified in one location. All defaults can still be overridden via
+parameters when instantiating classes.
+"""
+
+DEFAULT_MODEL = "openai:gpt-5-nano"
