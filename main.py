d203477570bf401386c7096314fc740c8f64983a
Jesse Palmer <jesselpalmer@gmail.com>
2026-01-25 12:11:02 -0800

    refactor: logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.agents.cfb_agent import CfbAgent
+from src.core import parse_args, setup_logging
 
 
 def main():
@@ -7,7 +8,13 @@ def main():
 
     Creates an instance of CfbAgent and processes user queries.
     """
-    user_message = "Hi, can you give me the scores on December 6th, 2025?"
+    args = parse_args()
+    setup_logging(args.log_level)
+
+    user_message = """
+    Hi, can you give me the results of games that the Hokies played within 12 months 
+    of December 6th, 2025?
+    """
 
     # Create the main agent
     agent = CfbAgent()
